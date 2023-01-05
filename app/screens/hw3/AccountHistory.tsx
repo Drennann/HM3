52ffafa9ed913e9fb06d04fe6e8cd9b197fe789c
Drennann <delpan.drennan@gmail.com>
2023-01-05 15:41:05 -0300

    first commit

---
+++ app/screens/hw3/AccountHistory.tsx
@@ -0,0 +1,11 @@
+import { View, Text } from "react-native";
+import React from "react";
+import { spacing, colors } from "../../theme";
+
+export function AccountHistory (){
+    return(
+        <View>
+            <Text>Account History</Text>
+        </View>
+    )
+}
